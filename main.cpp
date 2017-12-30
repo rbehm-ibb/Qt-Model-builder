ae4309e7c6fb89fc53574c5f0edaa1ecf757fbb0
Reinhardt Behm <rbehm@hushmail.com>
2017-12-30 17:15:21 +0800

    new version number 1.2

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	app.setApplicationName("model-builder");
-	app.setApplicationVersion("V1.1");
+	app.setApplicationVersion("V1.2");
 	app.setOrganizationDomain("avioscout.net");
 	app.setOrganizationName("R.Behm");
 	{
